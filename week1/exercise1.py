de89150eeae9d19b2b15f9643b9ef920b71ea5e4
osca1011 <36873754+osca1011@users.noreply.github.com>
2018-02-27 21:24:18 +1100

    homework
    sorry sir
    i have  just send the wrong file to you
    that's my fault

--- week1/exercise1.py
+++ week1/exercise1.py
@@ -1,5 +1,5 @@
 """Your very first python program!
 
-TODO: write a python script that first prints "hello world!"
+c: write a python script that first prints "hello world!"
 """
 print('Hello world!')
